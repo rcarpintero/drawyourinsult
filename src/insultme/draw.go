c3487dc9215046890d5344fcd792569c8ac51516
rubencg <Ruben.CarpinteroGago@zooplus.com>
2017-06-08 23:31:39 +0200

    First commit (awesomeski)

---
+++ src/insultme/draw.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "bufio"
+    "math/rand"
+    "time"
+    "os"
+    "encoding/json"
+    "log"
+    "net/http"
+ 
+    "github.com/gorilla/mux"
+)
+
+var insults []string
+
+func loadInsults() {
+    file, _ := os.Open("insult_list")
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        insults = append(insults, scanner.Text())
+    }
+//    log.Fatal(insults)
+}
+
+func random(max int) int {
+    rand.Seed(time.Now().Unix())
+    return rand.Intn(max)
+}
+
+func InsultEndpoint(w http.ResponseWriter, req *http.Request) {
+    insult := insults[random(len(insults) - 1)]
+    json.NewEncoder(w).Encode(insult)
+}
+
+func main() {
+    loadInsults()
+    router := mux.NewRouter()
+    router.HandleFunc("/drawinsult", InsultEndpoint).Methods("GET")
+    log.Fatal(http.ListenAndServe(":9090", router))
+}
